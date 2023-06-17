0fe67ac220ba63f551035e4db6b86ff815189353
inthelabAdmin <inthelab.android@gmail.com>
2023-06-17 17:23:44 -0400

    add about us link

--- lib/pages/settings/settings_helper.dart
+++ lib/pages/settings/settings_helper.dart
@@ -9,13 +9,14 @@ class SettingsListCreator {
     list.add(SettingsThemeItem());
    // list.add(SettingsSwitchItem('assets/icons/settings_notification.png',"Notifications", true));
 
+    list.add(SettingsWebLinkItem('assets/icons/settings_service.png', "Terms of Service", "https://prod-nch-client-web.herokuapp.com/termsOfLegalServices/mobile"));
+    list.add(SettingsWebLinkItem('assets/icons/settings_service.png',"Privacy Policy", "https://prod-nch-client-web.herokuapp.com/privacyPolicy/mobile"));
+    list.add(SettingsWebLinkItem('assets/icons/settings_link.png',"About Us", "https://prod-nch-client-web.herokuapp.com/aboutUs/mobile"));
+
     String email = Uri.encodeComponent("info@nationalcalendarhub.com");
     String subject = Uri.encodeComponent("Contact Us - Mobile User");
     String body = Uri.encodeComponent("[Enter your message here]");
     list.add(SettingsWebLinkItem('assets/icons/settings_contact.png',"Contact Us", "mailto:$email?subject=$subject&body=$body"));
-
-    list.add(SettingsWebLinkItem('assets/icons/settings_service.png', "Terms of Service", "https://prod-nch-client-web.herokuapp.com/termsOfLegalServices/mobile"));
-    list.add(SettingsWebLinkItem('assets/icons/settings_link.png',"Privacy Policy", "https://prod-nch-client-web.herokuapp.com/privacyPolicy/mobile"));
 
     return list;
   }
