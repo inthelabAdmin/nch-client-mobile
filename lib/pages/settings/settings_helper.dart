833421a96b375f62851407561f6ed09457663c45
inthelabAdmin <inthelab.android@gmail.com>
2023-07-01 20:12:07 -0400

    update url launcher package

--- lib/pages/settings/settings_helper.dart
+++ lib/pages/settings/settings_helper.dart
@@ -4,14 +4,13 @@ import 'package:national_calendar_hub_app/widgets/settings_list_item.dart';
 class SettingsListCreator {
   List<SettingsListItem> generateList() {
     final List<SettingsListItem> list = [];
-    //TODO add correct weblinks
     list.add(SettingHeaderItem("Settings"));
     list.add(SettingsThemeItem());
    // list.add(SettingsSwitchItem('assets/icons/settings_notification.png',"Notifications", true));
 
-    list.add(SettingsWebLinkItem('assets/icons/settings_service.png', "Terms of Service", "https://prod-nch-client-web.herokuapp.com/termsOfLegalServices/mobile"));
-    list.add(SettingsWebLinkItem('assets/icons/settings_service.png',"Privacy Policy", "https://prod-nch-client-web.herokuapp.com/privacyPolicy/mobile"));
-    list.add(SettingsWebLinkItem('assets/icons/settings_link.png',"About Us", "https://prod-nch-client-web.herokuapp.com/aboutUs/mobile"));
+    list.add(SettingsWebLinkItem('assets/icons/settings_service.png', "Terms of Service", "https://www.nationalcalendarhub.com/termsOfLegalServices/mobile"));
+    list.add(SettingsWebLinkItem('assets/icons/settings_service.png',"Privacy Policy", "https://www.nationalcalendarhub.com/privacyPolicy/mobile"));
+    list.add(SettingsWebLinkItem('assets/icons/settings_link.png',"About Us", "https://www.nationalcalendarhub.com/aboutUs/mobile"));
 
     String email = Uri.encodeComponent("info@nationalcalendarhub.com");
     String subject = Uri.encodeComponent("Contact Us - Mobile User");
